b338ac8c51b649c3284a92a55476aad90cb0f897
Theara Seng <me@thearaseng.com>
2023-10-24 14:06:43 -0500

    default 0 for numberOfBidders

--- src/main/java/com/waaproject/waaprojectbackend/dto/response/AuctionResponse.java
+++ src/main/java/com/waaproject/waaprojectbackend/dto/response/AuctionResponse.java
@@ -18,7 +18,7 @@ public class AuctionResponse {
     private LocalDateTime bidDueDateTime;
     private LocalDate payDate;
     private Boolean isEnd;
-    private Integer numberOfBidders;
+    private int numberOfBidders;
 
     private List<BidResponse> bids;
 }
