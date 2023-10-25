d4e651a1c5f8000c63dade24e16a0caec531ed99
Theara Seng <me@thearaseng.com>
2023-10-24 21:13:35 -0500

    fix

--- src/main/java/com/waaproject/waaprojectbackend/dto/response/UserResponse.java
+++ src/main/java/com/waaproject/waaprojectbackend/dto/response/UserResponse.java
@@ -1,14 +1,14 @@
 package com.waaproject.waaprojectbackend.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
 
     private String email;
